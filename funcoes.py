18cb52f8a1ea347a79093d79956df9e764f066b4
Cauã <cauasene00@hotmail.com>
2021-08-06 20:26:51 -0300

    (06/08) Funções e início da busca sequencial

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,5 @@
 # coding: latin-1
+from math import pi
 
 #Função é um trecho de código que tem um nome e pode receber informações externas para fazer seu trabalho.
 #Opcionalmente, uma função pode retornar um valor
@@ -17,3 +18,28 @@ p = float(input("Informe seu peso: "))
 a = float(input("Informe a altura: "))
 
 print(imc(p,a))
+
+
+def area_forma(base, altura, forma = "R"):
+    """
+        Função que calcula a área de uma das seguintes formas geométricas:
+        retangulo, triangulo ou elipse
+        Parametro forma:
+        "R" == retangulo
+        "T" == triangulo
+        "E" == elipse
+    """
+
+    area = 0
+    
+    if forma == "R":
+        area = base * altura
+    elif forma == "T":
+        area = base * altura / 2
+    elif forma == "E":
+        area = (base/2) * (altura * 2) * pi
+
+    return area
+
+
+print(f"Retangulo 7.5x11: {area_forma(7.5, 11)}")
